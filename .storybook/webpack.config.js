56c74b4377330b61c13d7ffdbbc9300c5ae17a43
Dom Jackson <dom.jackson@bbc.co.uk>
2019-08-03 16:04:03 +0100

    Updated Storybook webpack config for info addon

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,16 +1,16 @@
+const path = require('path');
+
 module.exports = ({ config }) => {
   config.module.rules.push({
-    test: /\.(ts|tsx)$/,
+    test: /\.tsx?$/,
+    include: path.resolve(__dirname, '../src'),
     use: [
-      {
-        loader: require.resolve('ts-loader'),
-      },
-      // Optional
-      {
-        loader: require.resolve('react-docgen-typescript-loader'),
-      },
+      require.resolve('ts-loader'),
+      require.resolve('react-docgen-typescript-loader'),
     ],
   });
+
   config.resolve.extensions.push('.ts', '.tsx');
+
   return config;
 };

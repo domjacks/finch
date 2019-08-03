b083fa8f8970cce60001491dd855d136cc5ea52c
Dom Jackson <dom.jackson@bbc.co.uk>
2019-08-03 15:25:35 +0100

    Added section name to Button story

--- src/components/button/stories.tsx
+++ src/components/button/stories.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Button from './';
 
-storiesOf('Button', module).add('with text', () => (
+storiesOf('Components|Button', module).add('with text', () => (
   <Button>Hello Button</Button>
 ));

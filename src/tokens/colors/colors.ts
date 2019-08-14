0c9adb5dd44df4961975f676e59aec2bc9b034a8
Dom Jackson <dom.jackson@bbc.co.uk>
2019-08-14 21:27:21 +0100

    add body colours

--- src/tokens/colors/colors.ts
+++ src/tokens/colors/colors.ts
@@ -2,9 +2,11 @@ export const colors = {
   light: {
     primary: '#93c0c1',
     accent: 'white',
+    body: 'black',
   },
   dark: {
     primary: '#93c0c1',
     accent: 'black',
+    body: 'white',
   },
 };

f09cc624add9b431758bca7c0892876dacbe7241
Dom Jackson <dom.jackson@bbc.co.uk>
2023-09-30 18:36:01 +0100

    Add sass bits

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,7 @@
+import styles from './Button.module.scss'
+
 const Button = () => {
-    return <button>World</button>
+    return <button class={styles.button}>World</button>
 }
 
 export { Button }

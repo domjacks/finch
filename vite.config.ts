a6a17afa7a65dee22c141a1dda15e753a18129c2
Dom Jackson <me@domjackson.xyz>
2023-11-11 16:35:33 +0000

    Add svgr vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path'
 import preact from '@preact/preset-vite';
+import svgr from "vite-plugin-svgr";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact()],
+	plugins: [preact(), svgr()],
 	build: {
 		lib: {
 			// Could also be a dictionary or array of multiple entry points
